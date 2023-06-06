2a9eed4372f8b84c8cd5e53efb4d4c9d09f98921
bunnysamurai <pohljedi@gmail.com>
2023-06-06 00:46:14 -0400

    a sillier example

--- src/main_blink.cpp
+++ src/main_blink.cpp
@@ -114,7 +114,27 @@ int main()
     sleep_ms(1000);
     scroll_left(tile_buf, 1);
     sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
     scroll_up(tile_buf, 8);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
+    sleep_ms(1000);
+    scroll_left(tile_buf, 1);
     sleep_ms(1000);
   }
 }
